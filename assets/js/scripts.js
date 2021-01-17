b81c5e6e780f7ad3bcdb55adcab0fe7dd9f27dde
Shawn <rafshawn@yandex.com>
2021-01-17 16:41:14 -0330

    Glowed Update v1
    Major updates:
    - Added animations, fade in on scroll
    - Typography update
    - Added glow effect on headers
    
    To-Do:
    - (Maybe) Add typewriter effect/animation
    - Fix background from gradient to transitioning solids on scroll
    - Navbar logo change on hover
    - Glowing social media footer
    - Add meet the team and prize sections
    - Fix sponsors section
    - Responsivity update
    - Add eventbrite registration widget

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,11 +1,13 @@
+//
 // Accordion javascript
+//
 
 const items = document.querySelectorAll('.accordion a.question');
 
 function toggleAccordion(){
     this.classList.toggle('active');
-    for(let item of items) {
-        if(item !== this) {
+    for(let item of items){
+        if(item !== this){
             item.classList.remove('active');
             item.nextElementSibling.classList.remove('active');
         }
@@ -14,3 +16,32 @@ function toggleAccordion(){
 }
 
 items.forEach(item => item.addEventListener('click', toggleAccordion));
+
+//
+// Fade in on scroll
+//
+
+const fade = document.querySelectorAll('.fadeIn-scroll');
+const fadeOptions ={
+    threshold: 1,
+    rootMargin: '0px 0px -45px 0px'
+};
+const appearOnScroll = new IntersectionObserver(function(
+    entries,
+    fadeOnScroll
+){
+    entries.forEach(entry =>{
+        if(!entry.isIntersecting){
+            return;
+        }
+        else{
+            entry.target.classList.add("appear");
+            fadeOnScroll.unobserve(entry.target);
+        }
+    });
+},
+fadeOptions);
+
+fade.forEach(fade =>{
+    appearOnScroll.observe(fade);
+});
