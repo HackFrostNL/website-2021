89ac9ba03a6ef9df6e7d0bcdc8056b96c745eddb
Shawn <rafshawn@yandex.com>
2021-02-19 22:14:13 -0330

    Meet the Team Update
    Still work in progress

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -55,3 +55,17 @@ function toggleAccordion(){
 }
 
 items.forEach(item => item.addEventListener('click', toggleAccordion));
+
+//
+// Marquee Scroll
+//
+
+const root = document.documentElement;
+const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue("--marquee-elements-displayed");
+const marqueeContent = document.querySelector("ul.marquee-content");
+
+root.style.setProperty("--marquee-elements", marqueeContent.children.length);
+
+for(let i=0; i<marqueeElementsDisplayed; i++) {
+  marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
+}
